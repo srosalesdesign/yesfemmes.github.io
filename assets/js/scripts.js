236f34d4a8ffb8097321efc52937231f7562e488
Sandra Rosales <sandrozza@gmail.com>
2017-01-29 16:02:57 -0800

    about update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,12 @@
-;(function ($) {
-  'use strict';
-  var $body    = $('html, body'), // Define jQuery collection 
-      content  = $('#main').smoothState({
-        onStart : {
-          duration: 250,
-          render: function () {
-            content.toggleAnimationClass('is-exiting');
-            
-            // Scroll user to the top
-            $body.animate({ 'scrollTop': 0 });
 
-          }
-        }
-      }).data('smoothState');
-})(jQuery);
+$( document ).ready(function() {
+$( ".letter-wrapper" ).click(function() {
+  $( ".hp-about").toggleClass( "fadeinright" );
+});
+
+$( ".close" ).click(function() {
+  $( ".hp-about").toggleClass( "fadeinright").addClass("slide-out");
+});
+});
+
+
